f76386780da4007cf60a29d0ae5678cbf4e24fe0
2nayeon <leenayeon0915@gmail.com>
2025-11-07 14:38:18 +0900

    week09: Student List App v0.5(buile.gradle.kts)

--- AppWeek09/app/build.gradle.kts
+++ AppWeek09/app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures { // ViewVinding 활성화
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,6 +45,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation("androidx.recyclerview:recyclerview:1.4.0") // RecyclerView 사용하기 위해 해야하는 작업
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
